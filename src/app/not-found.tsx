644d5de975699a08743f67550fb13b3195e9418d
michael-m-2983 <michaelmccright2983@gmail.com>
2024-03-14 17:52:21 -0500

    Add error 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import { Image, Stack, Title } from "@mantine/core";
+
+export default function ErrorPage() {
+    return <Stack align="center" gap="sm">
+        <Title order={2}>Page not found! Oh no!</Title>
+        <Image src="/rous.gif" w={350}/>
+        <Title order={3}>The scouting app got eaten by a ROUS!</Title>
+    </Stack>;
+}
