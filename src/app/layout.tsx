71b48eaf0e94eab808a9e0fca0200dea0ace242d
michael-m-2983 <michaelmccright2983@gmail.com>
2024-03-14 17:41:00 -0500

    Moved application container to template component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,60 @@
-import type { Metadata } from "next";
-import { Arvo } from "next/font/google";
+"use client";
+
 import '@mantine/core/styles.css';
 
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+import { AppShell, Burger, Group, Image, UnstyledButton, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import React from "react";
 
-const arvo = Arvo({weight: "400", subsets: ["latin"]});
+function NavButton(props: {
+  children: any,
+  destination: string
+}) {
+  return <UnstyledButton style={{ display: 'block', padding: '8px' }} onClick={() => window.location.href = props.destination}>
+    {props.children}
+  </UnstyledButton>;
+}
 
+/**
+ * This component contains the application shell [navbar, burger menu, title]
+ */
+export function ContentLayout(props: {children: React.ReactNode}) {
+  const [opened, { toggle }] = useDisclosure();
 
-export const metadata: Metadata = {
-  title: "Team 2530 Scouting Portal",
-  description: "",
-};
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { desktop: !opened, mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+
+          <Group justify="space-between" style={{ flex: 1 }}>
+            <Group>
+              <Image src="/logos/black.png" w={30} />
+              <Text>Turbo Scout</Text>
+            </Group>
+
+            <Burger opened={opened} onClick={toggle} size="sm" />
+          </Group>
+        </Group>
+      </AppShell.Header>
+
+      <AppShell.Navbar py="md" px={4}>
+        <NavButton destination='/'>Home</NavButton>
+        <NavButton destination='/pit'>Pit Scouting</NavButton>
+        <NavButton destination='/match'>Match Scouting</NavButton>
+        <NavButton destination='/error'>Data Download</NavButton>
+      </AppShell.Navbar>
+
+      <AppShell.Main>
+        {props.children}
+      </AppShell.Main>
+    </AppShell>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -21,9 +65,14 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <ColorSchemeScript defaultColorScheme="light" />
+        <title>turbo-scout</title>
       </head>
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        <MantineProvider>
+          <ContentLayout>
+            {children}
+          </ContentLayout>
+        </MantineProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,5 @@
 "use client";
-import { AppShell, Burger, Group, Image, UnstyledButton, Text } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import Link from 'next/link';
-
-function NavButton(props: {
-  children: any,
-  destination: string
-}) {
-  return <UnstyledButton style={{ display: 'block', padding: '8px' }} onClick={() => window.location.href = props.destination}>
-    {props.children}
-  </UnstyledButton>;
-}
 
 export default function Home() {
-  const [opened, { toggle }] = useDisclosure();
-
-  return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{ width: 300, breakpoint: 'sm', collapsed: { desktop: !opened, mobile: !opened } }}
-      padding="md"
-    >
-      <AppShell.Header>
-        <Group h="100%" px="md">
-
-          <Group justify="space-between" style={{ flex: 1 }}>
-            <Group>
-              <Image src="/logos/black.png" w={30} />
-              <Text>Turbo Scout</Text>
-            </Group>
-
-            <Burger opened={opened} onClick={toggle} size="sm" />
-          </Group>
-        </Group>
-      </AppShell.Header>
-
-      <AppShell.Navbar py="md" px={4}>
-        <NavButton destination='/'>Home</NavButton>
-        <NavButton destination='/pit'>Pit Scouting</NavButton>
-        <NavButton destination='/match'>Match Scouting</NavButton>
-        <NavButton destination='/error'>Data Download</NavButton>
-      </AppShell.Navbar>
-
-      <AppShell.Main>
-        The core app is not yet finished at this time.
-      </AppShell.Main>
-    </AppShell>
-  );
+  return <p>Hello, world!</p>;
 }
