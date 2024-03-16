ccf8a982f032898e2328bfc48595f1f9b30e4d6a
michael-m-2983 <michaelmccright2983@gmail.com>
2024-03-16 17:46:19 -0500

    Change pit list to a table
    Checkboxes still need to be fixed

--- src/app/pit/page.tsx
+++ src/app/pit/page.tsx
@@ -2,13 +2,33 @@
 
 import React from "react"
 import { TurboContext } from "../lib/context"
-import { Stack } from "@mantine/core";
+import { Checkbox, Table } from "@mantine/core";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-export default function PitDisplay(){
+export default function PitDisplay() {
     const { teams } = React.useContext(TurboContext);
 
-    return <Stack gap="xs" align="center">
-        {teams?.map(team => <Link href={`/pit/${team['key'].substring(3)}`}>{team['nickname']}</Link>)}
-    </Stack>;
+    const router = useRouter();
+
+    //TODO: keep checkbox state after going to another page
+
+    return <Table stickyHeader stickyHeaderOffset={60} withColumnBorders striped>
+        <Table.Thead>
+            <Table.Tr>
+                <Table.Tr></Table.Tr>
+                <Table.Td>####</Table.Td>
+                <Table.Td>Name</Table.Td>
+                <Table.Td>Rookie Year</Table.Td>
+            </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+            {teams?.map(team => <Table.Tr>
+                <Table.Td><Checkbox /></Table.Td>
+                <Table.Td onClick={() => router.push(`/pit/${team['key'].substring(3)}`)}>{team['key'].substring(3)}</Table.Td>
+                <Table.Td onClick={() => router.push(`/pit/${team['key'].substring(3)}`)}>{team['nickname']}</Table.Td>
+                <Table.Td>{team['rookie_year']}</Table.Td>
+            </Table.Tr>)}
+        </Table.Tbody>
+    </Table>;
 }
