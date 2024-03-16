c621dd22e30450224dd8b89c88c8b6f508228987
michael-m-2983 <michaelmccright2983@gmail.com>
2024-03-16 15:45:08 -0500

    Added pit scouting selection menu

--- src/app/pit/page.tsx
+++ src/app/pit/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import React from "react"
 import { TurboContext } from "../lib/context"
+import { Stack } from "@mantine/core";
 
 export default function PitDisplay(){
-    const { currentEvent } = React.useContext(TurboContext);
+    const { teams } = React.useContext(TurboContext);
 
-    return <p>Event: {currentEvent}</p>;
+    return <Stack gap="xs" align="center">
+        {teams?.map(team => <p>{team['nickname']}</p>)}
+    </Stack>;
 }
