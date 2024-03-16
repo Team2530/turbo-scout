5232680665b596d9d51f66d5a7fb19610130d3b5
michael-m-2983 <michaelmccright2983@gmail.com>
2024-03-15 22:17:27 -0500

    Add setup modal and event selection menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,47 @@
 "use client";
 
+import { Modal, Select } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import React from 'react';
+
+const TBA_KEY: string = "KYyfzxvdzhHGSE6ENeT6H7sxMJsO7Gzp0BMEi7AE3nTR7pHSsmKOSKAblMInnSfw";
+
+function RegionalSelect(props: { tbaKey: string }) {
+
+  const [events, setEvents] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://www.thebluealliance.com/api/v3/events/2024",
+      {
+        headers: {
+          "X-TBA-Auth-Key": props.tbaKey
+        }
+      }
+    )
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data);
+        setEvents(data);
+      });
+  }, [props.tbaKey]);
+
+  return <Select
+        label="Regional"
+        placeholder='2024mnmi'
+        searchable
+        data={events.length == 0 ? ["Loading events..."] : events.map(event => event['key'] + ": " + event['short_name'])}
+      />;
+}
+
+function SetupModal(props: any) {
+  const [opened, { open, close }] = useDisclosure(true);
+
+  return <Modal opened={opened} onClose={close} title="Setup turbo-scout" centered withCloseButton={false} size="lg" overlayProps={{ blur: 1 }}>
+    {/* select regional - Is there anything else? */}
+    <RegionalSelect tbaKey={TBA_KEY} />
+  </Modal>;
+}
+
 export default function Home() {
-  return <p>Hello, world!</p>;
+  return <SetupModal />;
 }
