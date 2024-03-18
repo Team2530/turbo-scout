cfabdfaf054755d299920d36878892beb495bdc4
michael-m-2983 <michaelmccright2983@gmail.com>
2024-03-17 21:35:10 -0500

    Prepare for github pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 import nextPWA from 'next-pwa';
 
 const withPWA = nextPWA({ dest: "public" })
-const nextConfig = withPWA({output: 'export'});
+const nextConfig = withPWA({ 
+  output: 'export',
+  basePath: "/turbo-scout",
+  images: {
+    unoptimized: true
+  }
+});
 
 export default nextConfig;
