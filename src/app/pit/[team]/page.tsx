1811ba9f51f0a9346ee9f9bfbc9559f58ddc0f28
michael-m-2983 <michaelmccright2983@gmail.com>
2024-03-17 19:43:09 -0500

    Add pit scouting form

--- src/app/pit/[team]/page.tsx
+++ src/app/pit/[team]/page.tsx
@@ -1,8 +1,44 @@
 "use client";
 
 import { TurboContext } from "@/app/lib/context";
-import { Badge, Checkbox, Fieldset, FileInput, Group, NumberInput, Rating, SegmentedControl, Stack, TagsInput, TextInput, Textarea, Title } from "@mantine/core";
+import { Badge, Checkbox, Fieldset, FileInput, Group, NumberInput, Rating, SegmentedControl, Select, Stack, Stepper, TagsInput, TextInput, Textarea, Title } from "@mantine/core";
 import React from "react";
+import SEASON_CONFIG from "../../season_config.json";
+
+function PitQuestion(props: {question: any}) {
+    const question: any = props.question;
+
+    switch(question.type) {
+        case "boolean":
+            //TODO: Make the text bold so that it matches the other question types
+            return <Checkbox label={question.name} />
+        case "paragraph":
+            return <Textarea label={question.name} />
+        case "text":
+            return <TextInput label={question.name} />
+        case "number":
+            //TODO: units
+            return <NumberInput label={question.name} />
+        case "select":
+            return <Select label={question.name} data={question.choices}/>
+        default:
+            //TODO: photo input
+            return <p>Not supported: {question.type}</p>
+    }
+}
+
+function PitScoutingMenu(props: { team: any }) {
+
+    const [currentStep, setCurrentStep] = React.useState(1);
+
+    return <Stepper active={currentStep} onStepClick={setCurrentStep} orientation="vertical">
+        {Object.entries(SEASON_CONFIG).map(([category, questions]) => <Stepper.Step label={category}>
+            <Stack>
+                {questions.map(question => <PitQuestion question={question}/>)}
+            </Stack>
+        </Stepper.Step>)}
+    </Stepper>
+}
 
 export default function TeamPitScouting({ params }: { params: { team: string } }) {
     //TODO: validate team param
@@ -23,31 +59,7 @@ export default function TeamPitScouting({ params }: { params: { team: string } }
         </Group>
 
         <Fieldset legend="Pit Scouting">
-            <Checkbox
-                defaultChecked
-                label="Checkbox field"
-            />
-            <FileInput
-                label="File field"
-                description="Description I guess"
-                placeholder="Input placeholder"
-            />
-            <NumberInput 
-                label="Number field (A)"
-                description="This is a number"
-            />
-            <Rating />
-            <SegmentedControl data={["Option A", "Option B", "Option C"]}/>
-            <Textarea 
-                label="Paragraph field"
-                description="loooooooooong text"
-            />
-            <TextInput 
-                label="Text field"
-                description="shorter text"
-            />
-            <TagsInput label="Tag Input"/>
-            <p>Also have ComboBox [multi and single]</p>
+            <PitScoutingMenu team={team} />
         </Fieldset>
 
 
