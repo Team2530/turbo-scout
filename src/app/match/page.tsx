c7478fc2e4ccf1cbfce390db833b2c9b25234472
michael-m-2983 <michaelmccright2983@gmail.com>
2024-03-21 16:53:07 -0500

    Start of match scouting system

--- src/app/match/page.tsx
+++ src/app/match/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+import { Fieldset, NumberInput, Select, Stack } from "@mantine/core";
+import React from "react";
+import { TurboContext } from "../lib/context";
+
+function MatchScoutingForm() {
+    const [matchNumber, setMatchNumber] = React.useState(0);
+    const [teamNumber, setTeamNumber] = React.useState<number | undefined>(0);
+
+    const { teams } = React.useContext(TurboContext);
+
+
+    return <Fieldset legend="Match Scouting">
+        <NumberInput label="Match Number" value={matchNumber} onChange={(v: string | number) => setMatchNumber(Number(v))}/>
+        <Select label="Team" data={teams?.map(team => `${team['key'].substring(3)}: ${team['nickname']}`)} searchable/>
+    </Fieldset>
+}
+
 export default function MatchScoutingPage() {
-    return <p>Match Scouting!</p>;
+    return <Stack>
+        <MatchScoutingForm />
+    </Stack>;
 }
