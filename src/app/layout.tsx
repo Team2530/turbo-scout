ca77a32f5a13d3b31bb9c807d420551de3d50ed8
Spoopr <qnguyenhammel@gmail.com>
2024-03-21 19:51:53 -0500

    PWA support still needs caching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,33 @@ export default function RootLayout({
       <head>
         <ColorSchemeScript defaultColorScheme="dark" />
         <title>turbo-scout</title>
+
+        <meta name="application-name" content="turbo-scoute" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="turbo-scout" />
+        <meta name="description" content="scoute the turbo" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#56ea16" />
+
+        <link rel="apple-touch-icon" href="/turbo-scout/icons/120x120.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/turbo-scout/icons/152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/turbo-scout/icons/180x180.png" />
+
+        <link rel="manifest" href="/turbo-scout/manifest.json" />
+        <link rel="shortcut icon" href="/src/app/favicon.ico" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="turbo-scout" />
+        <meta name="twitter:description" content="scoute the turbo" />
+        <meta name="twitter:image" content="/turbo-scout/icons/192x192.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="turbo-scout" />
+        <meta property="og:description" content="scoute the turbo" />
+        <meta property="og:site_name" content="turbo-scout" />
+        <meta property="og:image" content="/turbo-scout/icons/120x120.png" />
+
       </head>
       <body>
         <MantineProvider theme={MANTINE_THEME} defaultColorScheme="dark">
