63f78d20f51f550c3e6637716de5ec54f6e9fc4d
michael-m-2983 <michaelmccright2983@gmail.com>
2024-03-24 20:57:05 -0500

    Add data storing in push route

--- src/app/api/push/route.ts
+++ src/app/api/push/route.ts
@@ -1,8 +1,32 @@
 /**
  * This route allows the client to send data with the server
  */
+import { existsSync, mkdirSync, writeFile } from "fs";
+import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    return NextResponse.json({message: "The push route is not yet finished!"});
+    return NextResponse.json({ "error_message": "You cannot GET this api route!" });
+}
+
+export async function POST(
+    req: NextApiRequest
+) {
+    const dataDir = "./turbo-data/";
+
+    if (!existsSync(dataDir)) {
+        mkdirSync(dataDir);
+    }
+
+    if (req.body == null) {
+        return NextResponse.json({ "error_message": "You must send some data!" });
+    }
+
+    //TODO: check if data is blank but not null, e.g. {} or []
+
+    const fileName = "data-" + btoa(new Date().toISOString()) + ".json";
+    writeFile(fileName, req.body, () => { });
+
+
+    return NextResponse.json({ "fileName": fileName });
 }
