b3611de1c8b422bc7a2528ea29a081e1d604a603
michael-m-2983 <michaelmccright2983@gmail.com>
2024-03-27 21:35:22 -0500

    Remove api routes

--- src/app/api/pull/route.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This route will return all of the data on the server.
- */
-import { NextResponse } from "next/server";
-
-export async function GET() {
-    return NextResponse.json({message: "The pull route is not yet finished!"});
-}
