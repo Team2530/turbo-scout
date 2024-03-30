ac88e8d5b8e03c44919e71a59e43f95fcd8e2e9e
michael-m-2983 <michaelmccright2983@gmail.com>
2024-03-30 18:15:05 -0500

    Add flatmappping to pull api route

--- src/app/api/pull/route.ts
+++ src/app/api/pull/route.ts
@@ -1,11 +1,23 @@
 /**
- * This route will return all of the data on the server.
+ * This route returns all data on the server.
  */
-import { readFile, readFileSync, readdirSync } from "fs";
+import { readFileSync, readdirSync } from "fs";
 import { NextResponse } from "next/server";
 
 export async function GET() {
     const dataDir = "./turbo-data/";
-    let files = readdirSync(dataDir);
-    return NextResponse.json(files.map((path: any) => readFileSync(dataDir+path, {encoding: 'utf-8', flag: 'r'})));
+
+    const files: string[] = readdirSync(dataDir);
+    let result: any[] = [];
+
+    files.map(filePath => {
+        const fileContents: string = readFileSync(dataDir + filePath, {encoding: 'utf-8', flag: 'r'});
+        const fileDataEntries: any[] = JSON.parse(fileContents);
+
+        for(let entry of fileDataEntries) {
+            result.push(entry);
+        }
+    });
+
+    return NextResponse.json(result);
 }
