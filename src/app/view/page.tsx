61171458ab53f647162099dd6ba6afe3f35b724e
michael-m-2983 <michaelmccright2983@gmail.com>
2024-03-31 18:55:38 -0500

    Add teams page

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -12,20 +12,22 @@ import { DonutChart } from '@mantine/charts';
 import SEASON_MATCH_CONFIG from "../match_season_config.json";
 import SEASON_PIT_CONFIG from "../pit_season_config.json";
 import { TurboContext } from "../lib/context";
+import { TBA_KEY } from "../lib/tba_api";
 
 function ProgressTab(props: { data: any[] }) {
+
     //TODO: pit map display
 
-    const { teams } = React.useContext(TurboContext);
-    const pitData: any[] = props.data.filter(entry => entry['type'] == 'pit');
+    const { teams, currentEvent } = React.useContext(TurboContext);
+    const pitData: any[] = props.data.filter(entry => entry['event'] == currentEvent).filter(entry => entry['type'] == 'pit');
     const teamsNotPitScouted: any[] | undefined = teams?.filter(team => pitData.find(entry => entry['team'] == team['key'].substring(3)) == undefined);
 
     const pitCompletionChart = <DonutChart data={[
-        {name: "Scouted Teams", value: (teams?.length || 1) - (teamsNotPitScouted?.length || 0), color: 'green'},
-        {name: "Uncouted Teams", value: (teamsNotPitScouted?.length || 0), color: 'red'},
+        { name: "Scouted Teams", value: (teams?.length || 1) - (teamsNotPitScouted?.length || 0), color: 'green' },
+        { name: "Uncouted Teams", value: (teamsNotPitScouted?.length || 0), color: 'red' },
     ]}
-    size={200}
-    thickness={40}/>
+        size={200}
+        thickness={40} />
 
     // key.ss(3), nickname, state_prov + ", " + country
     const pitUnscoutedList = <Table>
@@ -65,30 +67,7 @@ function EntryTab(props: { data: any[] }) {
 
     const viewEntry = (entry: any) => modals.open({
         title: "Entry viewer: " + entry['type'] + " entry for team " + entry['team'],
-        children: <Stack gap="xs">
-            <p>Scouter: {entry['user']}</p>
-            <p>Event: {entry['event']}</p>
-            <p>Timestamp: {entry['timestamp']}</p>
-            {entry['matchNumber'] && <p>Match Number: {entry['matchNumber']}</p>}
-            {Object.entries(entry['data']).map(([category, values]: any) => {
-                return <Stack key={category} gap="xs">
-                    <p>{category}</p>
-                    <ul>
-                        {Object.entries(values).map(([key, value]: any) => {
-                            // Handle images
-                            if (category == "Photos") {
-                                return <li key={key}>
-                                    <p>{key}</p>
-                                    {value.map((image: string) => <Image src={image} key={MD5(image).toString()} alt="" />)}
-                                </li>
-                            }
-
-                            return <li key={key}>{key}: {JSON.stringify(value)}</li>
-                        })}
-                    </ul>
-                </Stack>;
-            })}
-        </Stack>
+        children: <EntryViewer entry={entry} />
     });
 
     return <Table stickyHeader stickyHeaderOffset={60} highlightOnHover>
@@ -148,10 +127,10 @@ function ExportTab(props: { data: Array<any> }) {
 
                     result += JSON.stringify(
                         entry['data'][category][questionName])
-                            .replaceAll(",", ";")
-                            .replaceAll('"', "")
-                            .replaceAll("[", "")
-                            .replaceAll("]", "") 
+                        .replaceAll(",", ";")
+                        .replaceAll('"', "")
+                        .replaceAll("[", "")
+                        .replaceAll("]", "")
                         + ",";
                 });
             });
@@ -167,13 +146,90 @@ function ExportTab(props: { data: Array<any> }) {
     </Stack>;
 }
 
+function TeamsTab(props: { data: any[], tbaData: any[] }) {
+    return <Table>
+        <Table.Thead>
+            <Table.Th>Team #</Table.Th>
+            <Table.Th>Team Name</Table.Th>
+            <Table.Th>EPA</Table.Th>
+            <Table.Th>OPR</Table.Th>
+            <Table.Th>DPR</Table.Th>
+            <Table.Th>CCWM</Table.Th>
+        </Table.Thead>
+        {/* TODO */}
+    </Table>
+}
+
+function TeamViewer(props: { data: any[], tbaData: any[], team: any }) {
+    return <p>Team viewer for team: {props.team['key'] + props.team['nickname']}</p>
+}
+
+function EntryViewer(props: { entry: any }) {
+
+    const entry = props.entry;
+
+    return <Stack gap="xs">
+        <p>Scouter: {entry['user']}</p>
+        <p>Event: {entry['event']}</p>
+        <p>Timestamp: {entry['timestamp']}</p>
+        {entry['matchNumber'] && <p>Match Number: {entry['matchNumber']}</p>}
+        {Object.entries(entry['data']).map(([category, values]: any) => {
+            return <Stack key={category} gap="xs">
+                <p>{category}</p>
+                <ul>
+                    {Object.entries(values).map(([key, value]: any) => {
+                        // Handle images
+                        if (category == "Photos") {
+                            return <li key={key}>
+                                <p>{key}</p>
+                                {value.map((image: string) => <Image src={image} key={MD5(image).toString()} alt="" />)}
+                            </li>
+                        }
+
+                        return <li key={key}>{key}: {JSON.stringify(value)}</li>
+                    })}
+                </ul>
+            </Stack>;
+        })}
+    </Stack>
+}
+
 export default function ViewDataPage() {
 
+    const { currentEvent } = React.useContext(TurboContext);
     const [data, setData] = React.useState<any[]>([]);
+    const [tbaData, setTbaData] = React.useState<any[]>([]);
 
     React.useEffect(() => {
         getAllData(setData);
     }, []);
+
+    React.useEffect(() => {
+        if(currentEvent == undefined) return;
+
+        const kv_pairs = {
+            "insights": `https://www.thebluealliance.com/api/v3/event/${currentEvent}/insights`,
+            "oprs": `https://www.thebluealliance.com/api/v3/event/${currentEvent}/oprs`,
+            "rankings": `https://www.thebluealliance.com/api/v3/event/${currentEvent}/rankings`,
+            "matches": `https://www.thebluealliance.com/api/v3/event/${currentEvent}/matches`
+        };
+
+        let resultant: any = {};
+
+        Object.entries(kv_pairs).forEach(async ([key, url]) => {
+            console.log(`${key} <-- ${url}`);
+
+            fetch(url, {headers: {"X-TBA-Auth-Key": TBA_KEY}}).then(r => r.json()).then((data: any) => {
+                resultant[key] = data;
+
+                if(Object.values(resultant).length == Object.values(kv_pairs).length) {
+                    setTbaData(resultant);
+                    console.log("Done loading TBA data!");
+                }
+            });
+        });
+
+    }, [currentEvent, setTbaData]);
 
     const tabs = {
         /**
@@ -189,6 +245,7 @@ export default function ViewDataPage() {
          */
         "Pit Progress": <ProgressTab data={data} />,
         "Entries": <EntryTab data={data} />,
+        "Teams": <TeamsTab data={data} tbaData={tbaData} />,
         "Export": <ExportTab data={data} />
     };
 
