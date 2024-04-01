73a72843709b0719ac008e828b7d0a52ebb73052
michael-m-2983 <michaelmccright2983@gmail.com>
2024-04-01 12:15:00 -0500

    Ignore /api route in PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 import nextPWA from 'next-pwa';
 
-const withPWA = nextPWA({ dest: "public" })
+const withPWA = nextPWA({ dest: "public", buildExcludes: ["/api/push", "/api/pull"], globIgnores: ["/api/**"] })
 const nextConfig = withPWA({});
 
 export default nextConfig;
