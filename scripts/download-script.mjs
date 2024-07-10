f009f0f19189b6b27bd54739e711d2cf2fba4094
michael-m-2983 <michaelmccright2983@gmail.com>
2024-07-09 23:39:27 -0500

    Move robocat schema and script to tarball

--- scripts/download-script.mjs
+++
@@ -1,7 +0,0 @@
-import { readFileSync, writeFileSync } from "fs";
-
-const data = JSON.parse(readFileSync("./pull", {encoding: 'utf8'}));
-
-for(const [index, entry] of data.entries()) {
-  writeFileSync(`downloaded-${index}.json`, JSON.stringify(entry));
-}
