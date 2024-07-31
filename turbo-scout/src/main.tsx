92a3390b209eb9d3634fdf8ebe7dd59e67bee5e2
michael-m-2983 <michaelmccright2983@gmail.com>
2024-07-31 18:43:40 -0500

    Switch to nested router layout

--- turbo-scout/src/main.tsx
+++ turbo-scout/src/main.tsx
@@ -1,29 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <p>Root route</p>,
-    errorElement: <p>Error route!</p>
+    element: <div>Root route</div>,
+    errorElement: <p>Error route!</p>,
+    children: [
+      {
+        path: "/pit",
+        element: <p>Pit scouting</p>
+      },
+      {
+        path: "/match",
+        element: <p>match scouting</p>
+      },
+      {
+        path: "/share",
+        element: <p>share data</p>
+      }
+    ]
   },
-  {
-    path: "/pit",
-    element: <p>Pit scouting</p>
-  },
-  {
-    path: "/match",
-    element: <p>match scouting</p>
-  },
-  {
-    path: "/share",
-    element: <p>share data</p>
-  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </React.StrictMode>,
 )
