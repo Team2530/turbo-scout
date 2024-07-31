0589cfb7255af53ac13fa76e6167d14d7579d41d
michael-m-2983 <michaelmccright2983@gmail.com>
2024-07-31 17:49:01 -0500

    Wipe the repository

--- src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import { Image, Stack, Title } from "@mantine/core";
-
-export default function ErrorPage() {
-    return <Stack align="center" gap="sm">
-        <Title order={2}>Page not found! Oh no!</Title>
-        <Image src="rous.gif" w={350} alt="ROUS" />
-        <Title order={3}>The scouting app got eaten by a ROUS!</Title>
-    </Stack>;
-}
