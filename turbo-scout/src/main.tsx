aca7d06b97c4ccf8a82f59d54f64cbe79d033a9a
michael-m-2983 <michaelmccright2983@gmail.com>
2024-07-31 18:30:13 -0500

    Add react-router

--- turbo-scout/src/main.tsx
+++ turbo-scout/src/main.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>Root route</p>,
+    errorElement: <p>Error route!</p>
+  },
+  {
+    path: "/pit",
+    element: <p>Pit scouting</p>
+  },
+  {
+    path: "/match",
+    element: <p>match scouting</p>
+  },
+  {
+    path: "/share",
+    element: <p>share data</p>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
