5c5684a63b7a49df6067ff899ec1f1701b85f2c1
michael-m-2983 <michaelmccright2983@gmail.com>
2024-08-01 14:52:27 -0500

    Add category tabs to pit scouting

--- turbo-scout/src/pit.tsx
+++ turbo-scout/src/pit.tsx
@@ -1,8 +1,21 @@
-import { useLocalStorage } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
 import { BaseLayout } from "./Layout";
+import CATEGORIES from "./config/pit.json";
+import { Container, Tabs } from "@mantine/core";
 
 export default function PitPage() {
     return <BaseLayout>
-        <p>Pit scouting</p>
+        <Container size="xl">
+            <Tabs defaultValue={CATEGORIES[0].id}>
+
+                <Tabs.List>
+                    {CATEGORIES.map(category => <Tabs.Tab key={category.id} value={category.id}>{category.label}</Tabs.Tab>)}
+                </Tabs.List>
+
+                {CATEGORIES.map(category => <Tabs.Panel key={category.id} value={category.id}>
+                    Content here
+                </Tabs.Panel>)}
+            </Tabs>
+        </Container>
     </BaseLayout>
 }
