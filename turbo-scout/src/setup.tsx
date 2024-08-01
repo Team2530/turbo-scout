c438ae5cc9ad5152e4c248aea3d643277690b02d
michael-m-2983 <michaelmccright2983@gmail.com>
2024-08-01 14:35:30 -0500

    Finish setup page

--- turbo-scout/src/setup.tsx
+++ turbo-scout/src/setup.tsx
@@ -1,24 +1,40 @@
-import { Container, Fieldset, Select, Stack, Text } from "@mantine/core";
+import { Button, Center, Container, Select, Stack, Text } from "@mantine/core";
 import { BaseLayout } from "./Layout";
 import { IconUserCircle } from '@tabler/icons-react';
 import EVENT from "./config/event.json";
+import { useForm } from "@mantine/form";
+import { useLocalStorage } from "@mantine/hooks";
 
 export default function SetupPage() {
 
-    const scouters: string[] = EVENT.scouters;
+    const [configuration, setConfiguration] = useLocalStorage<any>({ key: "config", defaultValue: undefined });
+
+    const form = useForm({
+        mode: "uncontrolled"
+    });
+
+    const profileDisplay = configuration && <Stack align="center">
+        <Text>Welcome to Turbo Scout, {configuration['profile']}</Text>
+        <Button onClick={() => setConfiguration(undefined)}>Sign out</Button>
+    </Stack>
+
+    const setupForm = <Stack>
+        <Text ta="center">Turbo-Scout Setup</Text>
+
+        {/* TODO: scout groups and other configuration options */}
+        <Select
+            label="Select Profile"
+            leftSection={<IconUserCircle />}
+            data={EVENT.scouters}
+            {...form.getInputProps("profile")}
+        />
+
+        <Button onClick={() => setConfiguration(form.getValues())}>Save</Button>
+    </Stack>
 
     return <BaseLayout>
-        <Container>
-            <Stack>
-                <Text ta="center">Turbo-Scout Setup</Text>
-
-                {/* TODO: scout groups */}
-                <Select
-                    label="Select Profile"
-                    leftSection={<IconUserCircle />}
-                    data={scouters}
-                />
-            </Stack>
+        <Container size="sm">
+            {configuration ? profileDisplay : setupForm}
         </Container>
     </BaseLayout>
 }
