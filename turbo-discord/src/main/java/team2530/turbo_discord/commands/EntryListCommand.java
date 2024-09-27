c331589439bf4e4b2d8676821a437789061ff6f9
michael-m-2983 <michaelmccright2983@gmail.com>
2024-09-27 18:34:09 -0500

    Add team filter to turbo-discord entry command

--- turbo-discord/src/main/java/team2530/turbo_discord/commands/EntryListCommand.java
+++ turbo-discord/src/main/java/team2530/turbo_discord/commands/EntryListCommand.java
@@ -1,23 +1,35 @@
 package team2530.turbo_discord.commands;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import team2530.turbo_discord.Command;
+import team2530.turbo_discord.CommandOption;
+import team2530.turbo_discord.DataStore;
 import team2530.turbo_discord.Main;
 
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class EntryListCommand extends Command {
     public EntryListCommand() {
-        super("entrylist", "Display a (large) list of all the entries");
+        super("entries", "Display a list of entries", new CommandOption[]{
+                new CommandOption(OptionType.INTEGER, "team", "Filter by team number", false)
+        });
     }
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
-        final String contents = String.join("\n", Main.DATA_STORE.getEntries().stream()
+        Stream<DataStore.Entry> entries = Main.DATA_STORE.getEntries().stream();
+
+        if (event.getOption("team") != null)
+            entries = entries.filter(entry -> entry.getTeamNumber() == event.getOption("team", OptionMapping::getAsInt));
+
+        final String contents = String.join("\n", entries
                 .map(entry -> String.format("%s entry for team %d by %s",
                         entry.getType(), entry.getTeamNumber(), entry.getUser()))
                 .collect(Collectors.toList()));
 
-        event.reply(contents).queue();
+        event.reply("```\n" + contents + "\n```").queue();
     }
 }
