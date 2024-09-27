679dd05747b48ddc4047ad699be3b7962b433150
michael-m-2983 <michaelmccright2983@gmail.com>
2024-09-26 21:57:04 -0500

    Add better data display menu

--- turbo-scout/src/pages/share.tsx
+++ turbo-scout/src/pages/share.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { BaseLayout } from "../layout";
 import DISCORD_CONFIG from "../config/discord.json";
 
-import { Card, Center, Container, Group, Stack, ThemeIcon, UnstyledButton } from "@mantine/core";
+import { Accordion, Card, Center, Container, Group, Stack, Textarea, ThemeIcon, UnstyledButton } from "@mantine/core";
 import { IconBrandDiscordFilled, IconDownload, IconQrcode } from "@tabler/icons-react";
 import { TurboStore, md5, useTurboStore } from "../state";
 import download from "downloadjs";
@@ -79,7 +79,7 @@ export default function SharePage() {
         <Container size="md">
             <pre>
                 {/* TODO: display this in a better format */}
-                {JSON.stringify(state)}
+                <EntryDisplayList entries={state.entries} />
             </pre>
             <Center>
                 <Group>
@@ -88,6 +88,27 @@ export default function SharePage() {
             </Center>
         </Container>
     </BaseLayout>
+}
+
+function EntryDisplayList(props: { entries: any[] }) {
+    if (props.entries.length == 0) return <p>No entries to display.</p>
+    return <Accordion>
+        {props.entries.map(entry => <EntryDisplay entry={entry} />)}
+    </Accordion>
+}
+
+function EntryDisplay(props: { entry: any }) {
+    const { entry } = props;
+    const hash = md5(JSON.stringify(entry));
+
+    return <Accordion.Item key={hash} value={`${entry['type']} entry for team ${entry['team']}`}>
+        <Accordion.Control>{`${entry['type']} entry for team ${entry['team']}`}</Accordion.Control>
+        <Accordion.Panel>
+            <Textarea readOnly resize="vertical">
+                {JSON.stringify(entry, undefined, "\t")}
+            </Textarea>
+        </Accordion.Panel>
+    </Accordion.Item>
 }
 
 function MethodButton(props: { method: ShareMethod, state: TurboStore }) {
