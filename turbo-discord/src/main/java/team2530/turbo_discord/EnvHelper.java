5261fc7b38ccb824d99e7872bfaab9d8d8d5c577
michael-m-2983 <michaelmccright2983@gmail.com>
2024-09-27 16:43:56 -0500

    turbo-discord: Add .env file loader

---
+++ turbo-discord/src/main/java/team2530/turbo_discord/EnvHelper.java
@@ -0,0 +1,37 @@
+package team2530.turbo_discord;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class EnvHelper {
+
+    /**
+     * Gets the discord token from a .env file.
+     *
+     * @return The discord bot token
+     */
+    public static String getDiscordBotToken() {
+        final Path path = Paths.get(".env");
+        final String key = "DISCORD_BOT_TOKEN";
+
+        if (!path.toFile().exists())
+            throw new RuntimeException("Cannot load '.env' file! Please create a .env file with the discord bot token.");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
+            String line = reader.readLine();
+
+            while (line != null) {
+                if (line.startsWith(key)) return line.substring(key.length() + 1); // The +1 accounts for the equals sign
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load '.env' file! Make sure that it exists and has your discord bot token!", e);
+        }
+
+        throw new RuntimeException(String.format("Could not find '%s'!", key));
+    }
+
+}
--- turbo-discord/src/main/java/team2530/turbo_discord/Main.java
+++ turbo-discord/src/main/java/team2530/turbo_discord/Main.java
@@ -2,6 +2,7 @@ package team2530.turbo_discord;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Starting turbo-discord");
+        String token = EnvHelper.getDiscordBotToken();
+        System.out.println(token);
     }
 }
