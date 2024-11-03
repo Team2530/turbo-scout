082f07b72a445236265c13772461ae2b5056cb19
michael-m-2983 <michaelmccright2983@gmail.com>
2024-11-02 22:34:46 -0500

    Add strategy drawing save functionality

--- turbo-scout/src/pages/strategy.tsx
+++ turbo-scout/src/pages/strategy.tsx
@@ -1,11 +1,55 @@
-import { Excalidraw } from "@excalidraw/excalidraw";
-import { Container } from "@mantine/core";
+import { Excalidraw, exportToCanvas } from "@excalidraw/excalidraw";
+import { Button, Container, Select } from "@mantine/core";
 import { BaseLayout } from "../layout";
+import EVENT_CONFIG from "../config/event.json";
+import React from "react";
+import { FormStore, formStoreDefaults } from "../form";
+import { create } from "zustand";
+import { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types/types";
+import { useTurboStore } from "../state";
+import { Configuration } from "./setup";
+import { useLocalStorage } from "@mantine/hooks";
+
+const useStrategyStore = create<FormStore>(formStoreDefaults);
 
 export default function StrategyPage() {
+    const store = useStrategyStore();
+    const { setDataField, setTeam, clearAllData } = store;
+
+    const addEntry = useTurboStore(s => s.addEntry);
+
+    const [configuration, _] = useLocalStorage<Configuration | undefined>({ key: "config", defaultValue: undefined });
+
+    const [excalidrawAPI, setExcalidrawAPI] = React.useState<ExcalidrawImperativeAPI | null>(null);
+
     return <BaseLayout>
-        <Container size="xl" style={{height: "70vh"}}>
-            <Excalidraw theme="dark" />
+        <Container size="xl" style={{ height: "70vh" }}>
+            <Excalidraw theme="dark" excalidrawAPI={(api) => setExcalidrawAPI(api)} />
+
+            <Select label="Team" placeholder="Select a team" searchable data={EVENT_CONFIG.teams.map(team => ({
+                value: team.team_number.toString(),
+                label: `${team.team_number}: ${team.nickname}`
+            }))} onChange={(v) => setTeam(parseInt(v!))} />
+
+            <Button onClick={async () => {
+                const { width, height, zoom } = excalidrawAPI!.getAppState();
+
+                const canvas = await exportToCanvas({
+                    elements: excalidrawAPI!.getSceneElements(),
+                    files: excalidrawAPI!.getFiles(),
+                    appState: excalidrawAPI!.getAppState(),
+                    getDimensions: () => ({
+                        width: width / zoom.value,
+                        height: height / zoom.value
+                    })
+                });
+
+                setDataField("image", canvas.toDataURL("image/png"));
+
+                addEntry(addEntry({ ...store, type: "strategy", user: configuration?.profile, timestamp: new Date() }));
+                clearAllData();
+                excalidrawAPI?.updateScene({}); //TODO: this does not work properly
+            }}>Save</Button>
         </Container>
     </BaseLayout>
 }
