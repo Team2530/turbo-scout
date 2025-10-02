0d068ae53ad32d661d673ff6059adc02dcd88732
michael-m-2983 <116770284+michael-m-2983@users.noreply.github.com>
2025-10-02 10:58:42 -0500

    Add setup automation script changes

--- .github/setup.py
+++ .github/setup.py
@@ -1,14 +1,35 @@
 from argparse import ArgumentParser
-#TODO: from tbapy import TBA
+from yaml import load as load_yaml
+from tbapy import TBA
 
 parser = ArgumentParser()
 parser.add_argument("config", help="Path to a YAML config file")
 
 args = parser.parse_args()
 
-print("config_path =", args.config)
+config = yaml_load(open(args.config, "r").read())
 
-with open(args.config, "r") as fp:
-    config = fp.read()
+scouters = config.scouters
 
-    print(config)
+print("scouters=", scouters)
+
+#TODO: support adding manual regionals
+
+tba = TBA("KYyfzxvdzhHGSE6ENeT6H7sxMJsO7Gzp0BMEi7AE3nTR7pHSsmKOSKAblMInnSfw")
+info = tba.event(config.event)
+teams = tba.event_teams(config.event)
+
+obj = {
+    "event": config.event,
+    "info": info,
+    "teams": teams,
+    "scouters": scouters
+}
+
+with open("turbo-discord/src/main/resources/teams.json", "w") as fp:
+    json.dump(teams, fp)
+    print("Saved turbo-discord config")
+
+with open("turbo-scout/src/config/event.json", "w") as fp:
+    json.dump(obj, fp)
+    print("Saved turbo-scout config")
